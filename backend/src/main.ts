278379a13c87b076ccc71173f9bd6db47d82a34c
efortish <91025555+efortish@users.noreply.github.com>
2025-09-08 20:29:34 -0500

    fix: backend fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+let app;
+
+async function createApp() {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  }
+  return app;
 }
-bootstrap();
+
+export default async (req, res) => {
+  const app = await createApp();
+  const server = app.getHttpAdapter().getInstance();
+  return server(req, res);
+};
